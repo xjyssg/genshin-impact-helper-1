43f86e2b6840b67efa5f62d090880b647155d2a9
xjyssg <32999913+xjyssg@users.noreply.github.com>
2023-01-18 11:45:11 +0800

    Update genshin-os.py

--- genshin-os.py
+++ genshin-os.py
@@ -65,7 +65,7 @@ class Sign(Base):
 
     def run(self):
         info_list = self.get_info()
-        log.info(info_list: {info_list}')
+        log.info('info_list: {info_list}')
         message_list = []
         if info_list:
             today = info_list.get('data',{}).get('today')
